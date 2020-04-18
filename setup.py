f21622451f292d7363ce1dba16752401ce29bc86
Eugen Ciur <eugen@django-lessons.com>
2020-04-18 16:19:17 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mgclipboard",
-    version="0.1.4",
+    version="0.2.0",
     author="Eugen Ciur",
     author_email="eugen@papermerge.com",
     url="https://github.com/papermerge/mg-clipboard",
